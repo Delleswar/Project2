d1431d57e5abdda56fec6d57bc35b0aa44d920b8
Delleswar <kotyadadelleswar@gmail.com>
2023-08-05 14:53:25 +0530

    commit

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './GetStarted.css'
+
+const GetStarted = () => {
+  return (
+    <section className="g-wrapper">
+        <div className="paddings innerWidth g-container">
+            <div className="flecColCenter inner-container">
+                <span className='primaryText'>Get Started with Homyz <br /></span>
+                <span className='secondaryText'>Subscribe and find more attractive price quotes from us.
+                    <br />
+                    Find your residence soon
+                    <br />
+                </span>
+                <button className="button">
+                    <a href="abc@gmail.com">Get Started</a>
+                </button>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default GetStarted
